4a52312b4649530b2e6264f772bccd3a8a364d55
tolgaolgun42 <121184586+tolgaolgun42@users.noreply.github.com>
2025-07-14 15:19:51 +0300

    Added Link

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,6 @@ initializeDatabase().then(() => {
     const PORT = process.env.PORT || 3005;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
+        console.log('http://localhost:3005');
     });
 });
