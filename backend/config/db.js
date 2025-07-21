7a8c7d087b87ed88f4c5abba01bd85dc5f11a71e
tolgaolgun42 <121184586+tolgaolgun42@users.noreply.github.com>
2025-07-21 21:49:37 +0300

    Added new connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
+    });
 
 pool.on('connect', () => {
     console.log("Connected to PostgreSQL");
