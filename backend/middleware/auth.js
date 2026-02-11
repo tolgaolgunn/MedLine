88ca6098ae1a3343f0db70f75b4486b2eec06589
tolgaolgun42 <121184586+tolgaolgun42@users.noreply.github.com>
2026-02-11 08:55:03 +0300

    Auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 // Token authentication middleware
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'] || req.cookies.token || req.body.token || req.headers["Authorization"];
+  const authHeader = req.headers['authorization'] || req.cookies.token || req.headers["Authorization"];
   if(!authHeader){
     return res.status(401).json({
       success: false,
